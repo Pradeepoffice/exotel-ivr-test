4001431a68c1790a8ad39b9cc3e09bc6a0ce3e6e
Pradeepoffice <pradeep.office92@gmail.com>
2026-05-16 20:32:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Exotel API Running');
+});
+
 app.post('/customer/validate', (req, res) => {
 
     console.log('Exotel Request Received');
 
     console.log(req.body);
 
-    // Read customer number from Exotel payload
     const customerNumber = req.body.From;
 
-    // Sample registered numbers
     const registeredNumbers = [
         '09880847047',
         '09790571549'
     ];
 
-    // Validation Logic
     if (registeredNumbers.includes(customerNumber)) {
 
         return res.status(200).json({
@@ -28,4 +36,10 @@ app.post('/customer/validate', (req, res) => {
             customerNumber: customerNumber
         });
     }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
